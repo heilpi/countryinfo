c33a88fcd43e68f47468e2c016ad0c3e4f3af929
Helmi <76262020+heilpi@users.noreply.github.com>
2022-04-07 19:46:46 +0300

    Add axios for api connection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
-function App() {
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+  console.log('render', countries.length, 'countries')
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredSearch = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(filter.toLocaleLowerCase())
+  );
+  console.log(filteredSearch);
+
   return (
     <div>
         <p>
